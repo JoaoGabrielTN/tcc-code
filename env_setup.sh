5ad337f5ff2882686a0ee77084b0793ebc0fab9b
JoaoGabrielTN <joaogabrieltdonascimento@gmail.com>
2025-11-06 11:47:40 -0300

    iperf3

--- env_setup.sh
+++ env_setup.sh
@@ -3,5 +3,20 @@ tmux new-session -s tshark -d
 
 tmux send-keys -t mn:0 'sudo python3 tcc_topo.py' Enter
 sleep 40
-tmux send-keys -t mn:0 'sh ifconfig hwsim0 up' Enter
+
+while true; do
+    if ip link show hwsim0 2>/dev/null | grep -q "state UNKNOWN"; then
+        echo "Interface hwsim0 está UP"
+        break
+    else
+        echo "Interface hwsim0 não está UP, tentando levantar..."
+        tmux send-keys -t mn:0 'sh ifconfig hwsim0 up' Enter
+        sleep 5  # Aguarda 5 segundos antes de verificar novamente
+    fi
+done
+
+for i in {2..10}; do
+	tmux send-keys -t mn:0 'car${i} iperf3 -s &' Enter
+done
+
 tmux send-keys -t tshark:0 'tshark -i hwsim0 -w benign.pcap' Enter
--- tcc_topo.py
+++ tcc_topo.py
@@ -34,9 +34,9 @@ def topology():
 	#s0 = net.addSwitch('s0', cls=OVSKernelSwitch)
 
 	rsu1 = net.addAccessPoint('rsu1', cls=OVSKernelAP, mac='00:00:00:11:00:01', channel='5', position='2168,228,0', **kwargs)
-	#rsu2 = net.addAccessPoint('rsu2', cls=OVSKernelAP, mac='00:00:00:11:00:02', channel='5', position='1987,321,0', **kwargs)
 	rsu3 = net.addAccessPoint('rsu3', cls=OVSKernelAP, mac='00:00:00:11:00:03', channel='5', position='1903,364,0', **kwargs)
 	rsu4 = net.addAccessPoint('rsu4', cls=OVSKernelAP, mac='00:00:00:11:00:04', channel='5', position='1655,490,0', **kwargs)
+	rsu2 = net.addAccessPoint('rsu2', cls=OVSKernelAP, mac='00:00:00:11:00:02', channel='5', position='1487,621,0', **kwargs)
 	rsu5 = net.addAccessPoint('rsu5', cls=OVSKernelAP, mac='00:00:00:11:00:05', channel='5', position='1258,717,0', **kwargs)
 	rsu6 = net.addAccessPoint('rsu6', cls=OVSKernelAP, mac='00:00:00:11:00:06', channel='5', position='902,810,0', **kwargs)
 	rsu7 = net.addAccessPoint('rsu7', cls=OVSKernelAP, mac='00:00:00:11:00:07', channel='5', position='660,892,0', **kwargs)
@@ -53,9 +53,9 @@ def topology():
 	net.configureWifiNodes()
 	
 	net.addLink(rsu1, rsu3)
-	#net.addLink(rsu2, rsu3)
 	net.addLink(rsu3, rsu4)
-	net.addLink(rsu4, rsu5)
+	net.addLink(rsu4, rsu2)
+	net.addLink(rsu2, rsu5)
 	net.addLink(rsu5, rsu6)
 	net.addLink(rsu6, rsu7)
 	for car in net.cars:
@@ -69,7 +69,7 @@ def topology():
 	c0.start()
 	for rsu in net.aps:
 		rsu.start([c0])
-	sleep(5)
+
 	for id, car in enumerate(net.cars):
 		car.setIP('192.168.0.{}/24'.format(id+1), intf='{}'.format(car.wintfs[0].name))
 		car.setIP('192.168.1.{}/24'.format(id+1), intf='{}'.format(car.wintfs[1].name))
